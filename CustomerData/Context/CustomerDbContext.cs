7df2eef6a332e7807755a012bf9ff53ea67946dc
joselvisalidojr <joselvisalidojr@gmail.com>
2022-04-29 08:52:26 +0800

    Add project files.

---
+++ CustomerData/Context/CustomerDbContext.cs
@@ -0,0 +1,34 @@
+﻿using CustomerData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerData.Context
+{
+    public class CustomerDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        public CustomerDbContext() : this("Server=GMMBQG3;Database=CustomerDb;User Id=sa;Password=p@ssw0rd")
+        {
+
+        }
+
+        public CustomerDbContext(string connectiongString)
+        {
+            this._connectionString = connectiongString;
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(this._connectionString);
+            }
+        }
+    }
+}
